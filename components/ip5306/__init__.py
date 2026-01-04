fe58aefb97c18100ccb33a02d19f594bbba47234
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-04 15:34:22 +0100

    Update __init__.py

--- components/ip5306/__init__.py
+++ components/ip5306/__init__.py
@@ -1,17 +1,40 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor, i2c, sensor
-from esphome.const import CONF_ID, CONF_BATTERY_LEVEL, DEVICE_CLASS_BATTERY, ICON_BATTERY, UNIT_PERCENT
+from esphome.components import binary_sensor, i2c, sensor, switch, select
+from esphome.const import CONF_ID, CONF_BATTERY_LEVEL, DEVICE_CLASS_BATTERY, UNIT_PERCENT
 
 MULTI_CONF = True
 
-AUTO_LOAD = [ "binary_sensor", "sensor" ]
+AUTO_LOAD = [ "binary_sensor", "sensor", "switch", "select" ]
 
 ip5306_ns = cg.esphome_ns.namespace('ip5306')
 IP5306 = ip5306_ns.class_('IP5306', i2c.I2CDevice, cg.Component)
+IP5306Switch = ip5306_ns.class_('IP5306Switch', switch.Switch, cg.Component)
+IP5306Select = ip5306_ns.class_('IP5306Select', select.Select, cg.Component)
 
 CONF_CHARGER_CONNECTED = "charger_connected"
 CONF_CHARGE_FULL = "charge_full"
+
+# Nove konfiguracie
+CONF_CHARGER_ENABLE = "charger_enable"
+CONF_LOW_LOAD_SHUTDOWN = "low_load_shutdown"
+CONF_LIGHT_LOAD_SHUTDOWN_TIME = "light_load_shutdown_time"
+CONF_CHARGE_CUTOFF_VOLTAGE = "charge_cutoff_voltage"
+CONF_CHARGE_TERMINATION_CURRENT = "charge_termination_current"
+
+# Enumy pre C++ (musia sediet s ip5306.h)
+IP5306SwitchType = ip5306_ns.enum_("IP5306SwitchType")
+SWITCH_TYPES = {
+    CONF_CHARGER_ENABLE: IP5306SwitchType.IP5306_SWITCH_CHARGER_ENABLE,
+    CONF_LOW_LOAD_SHUTDOWN: IP5306SwitchType.IP5306_SWITCH_LOW_LOAD_SHUTDOWN,
+}
+
+IP5306SelectType = ip5306_ns.enum_("IP5306SelectType")
+SELECT_TYPES = {
+    CONF_LIGHT_LOAD_SHUTDOWN_TIME: IP5306SelectType.IP5306_SELECT_LIGHT_LOAD_SHUTDOWN_TIME,
+    CONF_CHARGE_CUTOFF_VOLTAGE: IP5306SelectType.IP5306_SELECT_CHARGE_CUTOFF_VOLTAGE,
+    CONF_CHARGE_TERMINATION_CURRENT: IP5306SelectType.IP5306_SELECT_CHARGE_TERMINATION_CURRENT,
+}
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
@@ -23,6 +46,15 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
         ),
         cv.Optional(CONF_CHARGER_CONNECTED): binary_sensor.binary_sensor_schema(),
         cv.Optional(CONF_CHARGE_FULL): binary_sensor.binary_sensor_schema(),
+        
+        # Switche
+        cv.Optional(CONF_CHARGER_ENABLE): switch.switch_schema(IP5306Switch),
+        cv.Optional(CONF_LOW_LOAD_SHUTDOWN): switch.switch_schema(IP5306Switch),
+        
+        # Selecty
+        cv.Optional(CONF_LIGHT_LOAD_SHUTDOWN_TIME): select.select_schema(IP5306Select),
+        cv.Optional(CONF_CHARGE_CUTOFF_VOLTAGE): select.select_schema(IP5306Select),
+        cv.Optional(CONF_CHARGE_TERMINATION_CURRENT): select.select_schema(IP5306Select),
     }
 ).extend(i2c.i2c_device_schema(0x75))
 
@@ -42,4 +74,28 @@ async def to_code(config):
     if CONF_CHARGE_FULL in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_CHARGE_FULL])
         cg.add(var.set_charge_full(sens))
+        
+    # Registracia switchov
+    for key, type_enum in SWITCH_TYPES.items():
+        if key in config:
+            conf = config[key]
+            sw = await switch.new_switch(conf)
+            cg.add(var.set_switch(type_enum, sw))
 
+    # Registracia selectov
+    for key, type_enum in SELECT_TYPES.items():
+        if key in config:
+            conf = config[key]
+            sel = await select.new_select(conf, options=[]) # Options sa nastavia v C++ alebo tu
+            
+            # Nastavime moznosti podla typu (musia sediet s C++ logikou)
+            options = []
+            if key == CONF_LIGHT_LOAD_SHUTDOWN_TIME:
+                options = ["8s", "32s", "16s", "64s"]
+            elif key == CONF_CHARGE_CUTOFF_VOLTAGE:
+                options = ["4.2V", "4.3V", "4.35V", "4.4V"]
+            elif key == CONF_CHARGE_TERMINATION_CURRENT:
+                options = ["200mA", "400mA", "500mA", "600mA"]
+            
+            cg.add(sel.set_traits(options))
+            cg.add(var.set_select(type_enum, sel))

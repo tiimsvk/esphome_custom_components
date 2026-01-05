fc39c5cd1080c63d05ccd77fba381f27e279886a
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 10:06:19 +0100

    Update ip5306.cpp

--- components/ip5306/ip5306.cpp
+++ components/ip5306/ip5306.cpp
@@ -14,7 +14,9 @@ static const uint8_t IP5306_REG_CHARGER_CTL1 = 0x21;
 static const uint8_t IP5306_REG_CHARGER_CTL2 = 0x22;
 static const uint8_t IP5306_REG_READ0 = 0x70;
 static const uint8_t IP5306_REG_READ1 = 0x71;
-static const uint8_t IP5306_REG_LEVEL = 0x72;
+//static const uint8_t IP5306_REG_LEVEL = 0x72; //load current
+ton//static const uint8_t IP5306_REG_READ_3   0x77; //button
+static const uint8_t IP5306_REG_LEVEL = 0x78;
 
 void IP5306::setup() {
   ESP_LOGCONFIG(TAG, "Setting up IP5306...");

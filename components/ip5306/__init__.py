598d68edc21f1c136baba9ed9ce80dd804e3df1b
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 08:09:43 +0100

    Add voltage and current configuration options

--- components/ip5306/__init__.py
+++ components/ip5306/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor, i2c, sensor, switch, select
-from esphome.const import CONF_ID, CONF_BATTERY_LEVEL, DEVICE_CLASS_BATTERY, UNIT_PERCENT
+from esphome.const import CONF_ID, UNIT_VOLT, UNIT_AMPERE, DEVICE_CLASS_VOLTAGE, DEVICE_CLASS_CURRENT
 
 ip5306_ns = cg.esphome_ns.namespace("ip5306")
 IP5306 = ip5306_ns.class_("IP5306", i2c.I2CDevice, cg.Component)
@@ -12,14 +12,15 @@ AUTO_LOAD = ["binary_sensor", "sensor", "switch", "select"]
 
 CONF_CHARGER_CONNECTED = "charger_connected"  # NEW: Nabíjačka pripojená
 CONF_CHARGE_FULL = "charge_full"             # NEW: Nabíjanie dokončené
-
-# Nové konfigurácie
 CONF_LOW_LOAD_SHUTDOWN = "low_load_shutdown"
 CONF_CHARGER_ENABLE = "charger_enable"
 CONF_CHARGER_CONTROL = "charger_control"
 CONF_LOAD_SHUTDOWN_TIME = "load_shutdown_time"
 CONF_CHARGE_CUTOFF_VOLTAGE = "charge_cutoff_voltage"
 CONF_CHARGE_TERMINATION_CURRENT = "charge_termination_current"
+CONF_VOLTAGE = "voltage"
+CONF_CURRENT = "current"
+CONF_BOOST_CONTROL = "boost_control"
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
@@ -30,19 +31,29 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
             device_class=DEVICE_CLASS_BATTERY,
             accuracy_decimals=0,
         ),
+        cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_VOLT,
+            device_class=DEVICE_CLASS_VOLTAGE,
+            accuracy_decimals=2,
+        ),
+        cv.Optional(CONF_CURRENT): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE,
+            device_class=DEVICE_CLASS_CURRENT,
+            accuracy_decimals=2,
+        ),
         cv.Optional(CONF_CHARGER_CONNECTED): binary_sensor.binary_sensor_schema(),  # NEW
         cv.Optional(CONF_CHARGE_FULL): binary_sensor.binary_sensor_schema(),        # NEW
         # Switche
         cv.Optional(CONF_LOW_LOAD_SHUTDOWN): switch.switch_schema(IP5306Switch),
         cv.Optional(CONF_CHARGER_ENABLE): switch.switch_schema(IP5306Switch),
         cv.Optional(CONF_CHARGER_CONTROL): switch.switch_schema(IP5306Switch),
+        cv.Optional(CONF_BOOST_CONTROL): switch.switch_schema(),
         # Selecty
         cv.Optional(CONF_LOAD_SHUTDOWN_TIME): select.select_schema(IP5306Select),
         cv.Optional(CONF_CHARGE_CUTOFF_VOLTAGE): select.select_schema(IP5306Select),
         cv.Optional(CONF_CHARGE_TERMINATION_CURRENT): select.select_schema(IP5306Select),
     }
 ).extend(i2c.i2c_device_schema(0x75))
-
 
 async def to_code(config):
     # Inicializácia hlavnej triedy
@@ -98,3 +109,14 @@ async def to_code(config):
         cg.add(sel.set_parent(var))  # Nastav nadradenú triedu
         cg.add(sel.traits.set_options(["200mA", "400mA", "500mA", "600mA"]))
         cg.add(var.set_charge_termination_current_select(sel))
+    if CONF_VOLTAGE in config:
+        sens = await sensor.new_sensor(config[CONF_VOLTAGE])
+        cg.add(var.set_voltage_sensor(sens))
+
+    if CONF_CURRENT in config:
+        sens = await sensor.new_sensor(config[CONF_CURRENT])
+        cg.add(var.set_current_sensor(sens))
+
+    if CONF_BOOST_CONTROL in config:
+        sw = await switch.new_switch(config[CONF_BOOST_CONTROL])
+        cg.add(var.set_boost_control_switch(sw))

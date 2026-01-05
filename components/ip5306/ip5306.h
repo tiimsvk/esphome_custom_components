96927fbf2927b62210e3a12783dc5c4008f99a13
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 10:54:50 +0100

    Update ip5306.h

--- components/ip5306/ip5306.h
+++ components/ip5306/ip5306.h
@@ -7,6 +7,7 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/select/select.h"
+#include <vector>
 
 namespace esphome {
 namespace ip5306 {
@@ -17,6 +18,10 @@ enum IP5306SwitchType {
   IP5306_SWITCH_CHARGE_CONTROL,
   IP5306_SWITCH_BOOST_ENABLE,
   IP5306_SWITCH_SOFTWARE_SHUTDOWN,
+  // Tieto museli chýbať, preto hlásilo chybu:
+  IP5306_SWITCH_LOW_BAT_SHUTDOWN, 
+  IP5306_SWITCH_BOOST_ON_LOAD,
+  IP5306_SWITCH_BUTTON_SHUTDOWN
 };
 
 enum IP5306SelectType {
@@ -55,74 +60,65 @@ class IP5306 : public PollingComponent, public i2c::I2CDevice {
   void set_battery_level(sensor::Sensor *sensor) { this->battery_level_ = sensor; }
   void set_current_sensor(sensor::Sensor *current_sensor) { this->current_sensor_ = current_sensor; }
   void set_load_status_sensor(text_sensor::TextSensor *sensor) { this->load_status_sensor_ = sensor; }
-
   void set_charger_connected(binary_sensor::BinarySensor *sensor) { this->charger_connected_ = sensor; }
   void set_charge_full(binary_sensor::BinarySensor *sensor) { this->charge_full_ = sensor; }
 
-  // Setters pre Switche (presne tak, ako ich hlada main.cpp)
+  // Setters pre Switche (naplnaju vector switches_)
   void set_low_load_shutdown_switch(IP5306Switch *sw) { 
       sw->set_type(IP5306_SWITCH_LOW_LOAD_SHUTDOWN);
-      this->low_load_shutdown_switch_ = sw; 
+      this->switches_.push_back(sw);
   }
   void set_charger_enable_switch(IP5306Switch *sw) { 
       sw->set_type(IP5306_SWITCH_CHARGER_ENABLE);
-      this->charger_enable_switch_ = sw; 
+      this->switches_.push_back(sw);
   }
   void set_charge_control_switch(IP5306Switch *sw) { 
       sw->set_type(IP5306_SWITCH_CHARGE_CONTROL);
-      this->charge_control_switch_ = sw; 
+      this->switches_.push_back(sw);
   }
   void set_boost_control_switch(IP5306Switch *sw) { 
       sw->set_type(IP5306_SWITCH_BOOST_ENABLE);
-      this->boost_control_switch_ = sw; 
+      this->switches_.push_back(sw);
   }
   void set_software_shutdown_switch(IP5306Switch *sw) { 
       sw->set_type(IP5306_SWITCH_SOFTWARE_SHUTDOWN);
-      this->software_shutdown_switch_ = sw; 
+      this->switches_.push_back(sw);
   }
 
-  // Setters pre Selecty
+  // Setters pre Selecty (naplnaju vector selects_)
   void set_load_shutdown_time_select(IP5306Select *sel) {
     sel->set_type(IP5306_SELECT_LOAD_SHUTDOWN_TIME);
-    this->load_shutdown_time_select_ = sel;
+    this->selects_.push_back(sel);
   }
   void set_charge_cutoff_voltage_select(IP5306Select *sel) {
     sel->set_type(IP5306_SELECT_CHARGE_CUTOFF_VOLTAGE);
-    this->charge_cutoff_voltage_select_ = sel;
+    this->selects_.push_back(sel);
   }
   void set_charge_termination_current_select(IP5306Select *sel) {
     sel->set_type(IP5306_SELECT_CHARGE_TERMINATION_CURRENT);
-    this->charge_termination_current_select_ = sel;
+    this->selects_.push_back(sel);
   }
+  
+  // Verejne pre pristup z cpp
+  std::vector<IP5306Select *> selects_;
+  std::vector<IP5306Switch *> switches_;
 
   void write_register_bit(uint8_t reg, uint8_t mask, bool value);
   void write_register_bits(uint8_t reg, uint8_t mask, uint8_t shift, uint8_t value);
 
  private:
-  sensor::Sensor *current_sensor_{nullptr};
   sensor::Sensor *battery_level_{nullptr};
+  sensor::Sensor *current_sensor_{nullptr};
   text_sensor::TextSensor *load_status_sensor_{nullptr};
-  
   binary_sensor::BinarySensor *charger_connected_{nullptr};
   binary_sensor::BinarySensor *charge_full_{nullptr};
-  
-  IP5306Switch *low_load_shutdown_switch_{nullptr};
-  IP5306Switch *charger_enable_switch_{nullptr};
-  IP5306Switch *charge_control_switch_{nullptr};
-  IP5306Switch *boost_control_switch_{nullptr};
-  IP5306Switch *software_shutdown_switch_{nullptr};
-  
-  IP5306Select *load_shutdown_time_select_{nullptr};
-  IP5306Select *charge_cutoff_voltage_select_{nullptr};
-  IP5306Select *charge_termination_current_select_{nullptr};
-  
-  // Premenne pre zabranenie spamu v logu
+
+  // Premenne pre ukladanie stavu
   float last_battery_level_{-1};
   float last_current_{-1};
   std::string last_load_status_{""};
-  bool last_charger_connected_{false};
-  bool last_charge_full_{false};
-  bool first_update_{true};
+  int last_charger_connected_{-1};
+  int last_charge_full_{-1};
 };
 
 }  // namespace ip5306

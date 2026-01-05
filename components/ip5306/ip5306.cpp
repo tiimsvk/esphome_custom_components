5b6749cd794c9e6dfa5d87c689322a90fd87b341
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 06:04:47 +0100

    Update ip5306.cpp

--- components/ip5306/ip5306.cpp
+++ components/ip5306/ip5306.cpp
@@ -1,73 +1,62 @@
-#pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/switch/switch.h"
-#include "esphome/components/select/select.h"
+#include "ip5306.h"
+#include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
 
 namespace esphome {
 namespace ip5306 {
 
-enum IP5306SwitchType {
-  IP5306_SWITCH_CHARGER_ENABLE,
-  IP5306_SWITCH_LOW_LOAD_SHUTDOWN,
-};
+static const char *const TAG = "ip5306";
 
-enum IP5306SelectType {
-  IP5306_SELECT_LIGHT_LOAD_SHUTDOWN_TIME,
-  IP5306_SELECT_CHARGE_CUTOFF_VOLTAGE,
-  IP5306_SELECT_CHARGE_TERMINATION_CURRENT,
-};
+// Implementácia pre prepínače
+void IP5306Switch::write_state(bool state) {
+  this->parent_->handle_switch_write(this->type_, state);
+}
 
-class IP5306;
+void IP5306Select::control(const std::string &value) {
+  this->parent_->handle_select_control(this->type_, value);
+}
 
-class IP5306Switch : public switch_::Switch, public Parented<IP5306> {
- public:
-  void write_state(bool state) override;
-  void set_type(IP5306SwitchType type) { this->type_ = type; }
- private:
-  IP5306SwitchType type_;
-};
+float IP5306::get_setup_priority() const { return setup_priority::IO; }
 
-class IP5306Select : public select::Select, public Parented<IP5306> {
- public:
-  void control(const std::string &value) override;
-  void set_type(IP5306SelectType type) { this->type_ = type; }
- private:
-  IP5306SelectType type_;
-};
+void IP5306::setup() {
+  // Pridanie logiky pre Select a Switch na inicializáciu
+  ESP_LOGD(TAG, "Initializing IP5306...");
+}
 
-class IP5306 : public i2c::I2CDevice, public PollingComponent {
- public:
-  IP5306() : PollingComponent(60000) {}
-  void setup() override;
-  void update() override;
+void IP5306::update() {
+  // Aktualizácia hodnôt zo senzorov
+  ESP_LOGD(TAG, "Updating IP5306...");
+}
 
-  float get_setup_priority() const override;
+void IP5306::set_switch(IP5306SwitchType type, IP5306Switch *sw) {
+  sw->set_parent(this);
+  sw->set_type(type);
+  switches_.push_back(sw);
+}
 
-  void set_battery_level(sensor::Sensor *sensor) { this->battery_level_ = sensor; }
-  void set_charger_connected(binary_sensor::BinarySensor *sensor) { this->charger_connected_ = sensor; }
-  void set_charge_full(binary_sensor::BinarySensor *sensor) { this->charge_full_ = sensor; }
+void IP5306::set_select(IP5306SelectType type, IP5306Select *sel) {
+  sel->set_parent(this);
+  sel->set_type(type);
+  selects_.push_back(sel);
+}
 
-  void set_switch(IP5306SwitchType type, IP5306Switch *sw);
-  void set_select(IP5306SelectType type, IP5306Select *sel);
+void IP5306::handle_switch_write(IP5306SwitchType type, bool state) {
+  // Logika zápisu do registru pre prepínače
+  ESP_LOGI(TAG, "Writing switch state...");
+}
 
-  void handle_switch_write(IP5306SwitchType type, bool state);
-  void handle_select_control(IP5306SelectType type, const std::string &value);
+void IP5306::handle_select_control(IP5306SelectType type, const std::string &value) {
+  // Logika zápisu do registru pre výberové komponenty
+  ESP_LOGI(TAG, "Handling select control...");
+}
 
- protected:
-  sensor::Sensor *battery_level_{nullptr};
-  binary_sensor::BinarySensor *charger_connected_{nullptr};
-  binary_sensor::BinarySensor *charge_full_{nullptr};
+void IP5306::update_register_bit_(uint8_t reg, uint8_t mask, bool value) {
+  // Pomocná funkcia zápisu do registra
+}
 
-  std::vector<IP5306Switch *> switches_;
-  std::vector<IP5306Select *> selects_;
-
-  void update_register_bit_(uint8_t reg, uint8_t mask, bool value);
-  void update_register_value_(uint8_t reg, uint8_t mask, uint8_t shift, uint8_t value);
-};
+void IP5306::update_register_value_(uint8_t reg, uint8_t mask, uint8_t shift, uint8_t value) {
+  // Pomocná funkcia zápisu do registra
+}
 
 }  // namespace ip5306
 }  // namespace esphome

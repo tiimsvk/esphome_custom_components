8ea727d9f29b22db21a0a6666b0c504a6fa4d4bb
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 06:36:32 +0100

    Update __init__.py

--- components/ip5306/__init__.py
+++ components/ip5306/__init__.py
@@ -3,10 +3,6 @@ import esphome.config_validation as cv
 from esphome.components import binary_sensor, i2c, sensor, switch, select
 from esphome.const import CONF_ID, CONF_BATTERY_LEVEL, DEVICE_CLASS_BATTERY, UNIT_PERCENT
 
-MULTI_CONF = True
-
-AUTO_LOAD = [ "binary_sensor", "sensor", "switch", "select" ]
-
 ip5306_ns = cg.esphome_ns.namespace('ip5306')
 IP5306 = ip5306_ns.class_('IP5306', i2c.I2CDevice, cg.Component)
 IP5306Switch = ip5306_ns.class_('IP5306Switch', switch.Switch, cg.Component)
@@ -15,24 +11,13 @@ IP5306Select = ip5306_ns.class_('IP5306Select', select.Select, cg.Component)
 CONF_CHARGER_CONNECTED = "charger_connected"
 CONF_CHARGE_FULL = "charge_full"
 
-# Nove konfiguracie
-CONF_CHARGER_ENABLE = "charger_enable"
+# Nové konfigurácie
 CONF_LOW_LOAD_SHUTDOWN = "low_load_shutdown"
-CONF_LIGHT_LOAD_SHUTDOWN_TIME = "light_load_shutdown_time"
+CONF_CHARGER_ENABLE = "charger_enable"
+CONF_CHARGER_CONTROL = "charger_control"  # NEW: Ovládanie nabíjania
+CONF_LOAD_SHUTDOWN_TIME = "load_shutdown_time"  # NEW: Čas vypnutia záťaže
 CONF_CHARGE_CUTOFF_VOLTAGE = "charge_cutoff_voltage"
 CONF_CHARGE_TERMINATION_CURRENT = "charge_termination_current"
-
-# Enumy pre C++ (Generujeme ako RAW stringy)
-SWITCH_TYPES = {
-    CONF_CHARGER_ENABLE: cg.RawExpression("ip5306::IP5306_SWITCH_CHARGER_ENABLE"),
-    CONF_LOW_LOAD_SHUTDOWN: cg.RawExpression("ip5306::IP5306_SWITCH_LOW_LOAD_SHUTDOWN"),
-}
-
-SELECT_TYPES = {
-    CONF_LIGHT_LOAD_SHUTDOWN_TIME: cg.RawExpression("ip5306::IP5306_SELECT_LIGHT_LOAD_SHUTDOWN_TIME"),
-    CONF_CHARGE_CUTOFF_VOLTAGE: cg.RawExpression("ip5306::IP5306_SELECT_CHARGE_CUTOFF_VOLTAGE"),
-    CONF_CHARGE_TERMINATION_CURRENT: cg.RawExpression("ip5306::IP5306_SELECT_CHARGE_TERMINATION_CURRENT"),
-}
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
@@ -42,15 +27,10 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
             device_class=DEVICE_CLASS_BATTERY,
             accuracy_decimals=0,
         ),
-        cv.Optional(CONF_CHARGER_CONNECTED): binary_sensor.binary_sensor_schema(),
-        cv.Optional(CONF_CHARGE_FULL): binary_sensor.binary_sensor_schema(),
-        
-        # Switche
-        cv.Optional(CONF_CHARGER_ENABLE): switch.switch_schema(IP5306Switch),
         cv.Optional(CONF_LOW_LOAD_SHUTDOWN): switch.switch_schema(IP5306Switch),
-        
-        # Selecty
-        cv.Optional(CONF_LIGHT_LOAD_SHUTDOWN_TIME): select.select_schema(IP5306Select),
+        cv.Optional(CONF_CHARGER_ENABLE): switch.switch_schema(IP5306Switch),
+        cv.Optional(CONF_CHARGER_CONTROL): switch.switch_schema(IP5306Switch),  # NEW
+        cv.Optional(CONF_LOAD_SHUTDOWN_TIME): select.select_schema(IP5306Select),  # NEW
         cv.Optional(CONF_CHARGE_CUTOFF_VOLTAGE): select.select_schema(IP5306Select),
         cv.Optional(CONF_CHARGE_TERMINATION_CURRENT): select.select_schema(IP5306Select),
     }
@@ -64,36 +44,32 @@ async def to_code(config):
     if CONF_BATTERY_LEVEL in config:
         sens = await sensor.new_sensor(config[CONF_BATTERY_LEVEL])
         cg.add(var.set_battery_level(sens))
-
-    if CONF_CHARGER_CONNECTED in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_CHARGER_CONNECTED])
-        cg.add(var.set_charger_connected(sens))
-
-    if CONF_CHARGE_FULL in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_CHARGE_FULL])
-        cg.add(var.set_charge_full(sens))
-        
-    # Registracia switchov
-    for key, type_enum in SWITCH_TYPES.items():
-        if key in config:
-            conf = config[key]
-            sw = await switch.new_switch(conf)
-            cg.add(var.set_switch(type_enum, sw))
-
-    # Registracia selectov
-    for key, type_enum in SELECT_TYPES.items():
-        if key in config:
-            conf = config[key]
-            sel = await select.new_select(conf, options=[])
-            
-            options = []
-            if key == CONF_LIGHT_LOAD_SHUTDOWN_TIME:
-                options = ["8s", "32s", "16s", "64s"]
-            elif key == CONF_CHARGE_CUTOFF_VOLTAGE:
-                options = ["4.2V", "4.3V", "4.35V", "4.4V"]
-            elif key == CONF_CHARGE_TERMINATION_CURRENT:
-                options = ["200mA", "400mA", "500mA", "600mA"]
-            
-            # Oprava: nastavenie options cez traits
-            cg.add(sel.traits.set_options(options))
-            cg.add(var.set_select(type_enum, sel))
+    
+    # Pridanie prepínačov
+    if CONF_LOW_LOAD_SHUTDOWN in config:
+        sw = await switch.new_switch(config[CONF_LOW_LOAD_SHUTDOWN])
+        cg.add(var.set_low_load_shutdown_switch(sw))
+    
+    if CONF_CHARGER_ENABLE in config:
+        sw = await switch.new_switch(config[CONF_CHARGER_ENABLE])
+        cg.add(var.set_charger_enable_switch(sw))
+    
+    if CONF_CHARGER_CONTROL in config:  # NEW
+        sw = await switch.new_switch(config[CONF_CHARGER_CONTROL])
+        cg.add(var.set_charge_control_switch(sw))
+    
+    # Pridanie selects
+    if CONF_LOAD_SHUTDOWN_TIME in config:  # NEW
+        sel = await select.new_select(config[CONF_LOAD_SHUTDOWN_TIME], options=[])
+        cg.add(sel.traits.set_options(["8s", "32s", "16s", "64s"]))
+        cg.add(var.set_load_shutdown_time_select(sel))
+    
+    if CONF_CHARGE_CUTOFF_VOLTAGE in config:
+        sel = await select.new_select(config[CONF_CHARGE_CUTOFF_VOLTAGE], options=[])
+        cg.add(sel.traits.set_options(["4.2V", "4.3V", "4.35V", "4.4V"]))
+        cg.add(var.set_charge_cutoff_voltage_select(sel))
+    
+    if CONF_CHARGE_TERMINATION_CURRENT in config:
+        sel = await select.new_select(config[CONF_CHARGE_TERMINATION_CURRENT], options=[])
+        cg.add(sel.traits.set_options(["200mA", "400mA", "500mA", "600mA"]))
+        cg.add(var.set_charge_termination_current_select(sel))

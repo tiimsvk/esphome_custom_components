688d5d10243c4a467e22a462425b40666de43630
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 06:37:49 +0100

    Add binary sensors for charger status
    Added binary sensors for charger connection and charge status.

--- components/ip5306/__init__.py
+++ components/ip5306/__init__.py
@@ -8,29 +8,34 @@ IP5306 = ip5306_ns.class_('IP5306', i2c.I2CDevice, cg.Component)
 IP5306Switch = ip5306_ns.class_('IP5306Switch', switch.Switch, cg.Component)
 IP5306Select = ip5306_ns.class_('IP5306Select', select.Select, cg.Component)
 
-CONF_CHARGER_CONNECTED = "charger_connected"
-CONF_CHARGE_FULL = "charge_full"
+CONF_CHARGER_CONNECTED = "charger_connected"  # NEW: Nabíjačka pripojená
+CONF_CHARGE_FULL = "charge_full"  # NEW: Nabíjanie dokončené
 
 # Nové konfigurácie
 CONF_LOW_LOAD_SHUTDOWN = "low_load_shutdown"
 CONF_CHARGER_ENABLE = "charger_enable"
-CONF_CHARGER_CONTROL = "charger_control"  # NEW: Ovládanie nabíjania
-CONF_LOAD_SHUTDOWN_TIME = "load_shutdown_time"  # NEW: Čas vypnutia záťaže
+CONF_CHARGER_CONTROL = "charger_control"  
+CONF_LOAD_SHUTDOWN_TIME = "load_shutdown_time"  
 CONF_CHARGE_CUTOFF_VOLTAGE = "charge_cutoff_voltage"
 CONF_CHARGE_TERMINATION_CURRENT = "charge_termination_current"
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(IP5306),
+        # Senzory
         cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
             unit_of_measurement=UNIT_PERCENT,
             device_class=DEVICE_CLASS_BATTERY,
             accuracy_decimals=0,
         ),
+        cv.Optional(CONF_CHARGER_CONNECTED): binary_sensor.binary_sensor_schema(),  # NEW
+        cv.Optional(CONF_CHARGE_FULL): binary_sensor.binary_sensor_schema(),  # NEW    
+        # Switche
         cv.Optional(CONF_LOW_LOAD_SHUTDOWN): switch.switch_schema(IP5306Switch),
         cv.Optional(CONF_CHARGER_ENABLE): switch.switch_schema(IP5306Switch),
-        cv.Optional(CONF_CHARGER_CONTROL): switch.switch_schema(IP5306Switch),  # NEW
-        cv.Optional(CONF_LOAD_SHUTDOWN_TIME): select.select_schema(IP5306Select),  # NEW
+        cv.Optional(CONF_CHARGER_CONTROL): switch.switch_schema(IP5306Switch), 
+        # Selecty
+        cv.Optional(CONF_LOAD_SHUTDOWN_TIME): select.select_schema(IP5306Select), 
         cv.Optional(CONF_CHARGE_CUTOFF_VOLTAGE): select.select_schema(IP5306Select),
         cv.Optional(CONF_CHARGE_TERMINATION_CURRENT): select.select_schema(IP5306Select),
     }
@@ -41,11 +46,21 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
+    # Battery level sensor
     if CONF_BATTERY_LEVEL in config:
         sens = await sensor.new_sensor(config[CONF_BATTERY_LEVEL])
         cg.add(var.set_battery_level(sens))
     
-    # Pridanie prepínačov
+    # Binary sensors
+    if CONF_CHARGER_CONNECTED in config:  # NEW
+        binary_sens = await binary_sensor.new_binary_sensor(config[CONF_CHARGER_CONNECTED])
+        cg.add(var.set_charger_connected(binary_sens))  # Add connection to C++
+    
+    if CONF_CHARGE_FULL in config:  # NEW
+        binary_sens = await binary_sensor.new_binary_sensor(config[CONF_CHARGE_FULL])
+        cg.add(var.set_charge_full(binary_sens))  # Add connection to C++
+
+    # Add switches
     if CONF_LOW_LOAD_SHUTDOWN in config:
         sw = await switch.new_switch(config[CONF_LOW_LOAD_SHUTDOWN])
         cg.add(var.set_low_load_shutdown_switch(sw))
@@ -54,12 +69,12 @@ async def to_code(config):
         sw = await switch.new_switch(config[CONF_CHARGER_ENABLE])
         cg.add(var.set_charger_enable_switch(sw))
     
-    if CONF_CHARGER_CONTROL in config:  # NEW
+    if CONF_CHARGER_CONTROL in config:  
         sw = await switch.new_switch(config[CONF_CHARGER_CONTROL])
         cg.add(var.set_charge_control_switch(sw))
     
-    # Pridanie selects
-    if CONF_LOAD_SHUTDOWN_TIME in config:  # NEW
+    # Add selects
+    if CONF_LOAD_SHUTDOWN_TIME in config:  
         sel = await select.new_select(config[CONF_LOAD_SHUTDOWN_TIME], options=[])
         cg.add(sel.traits.set_options(["8s", "32s", "16s", "64s"]))
         cg.add(var.set_load_shutdown_time_select(sel))

393fd9c6f1eb0a2fe9bf90a81f52273190f6aa96
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-05 10:32:16 +0100

    Update ip5306.h

--- components/ip5306/ip5306.h
+++ components/ip5306/ip5306.h
@@ -14,9 +14,10 @@ namespace ip5306 {
 enum IP5306SwitchType {
   IP5306_SWITCH_LOW_LOAD_SHUTDOWN,
   IP5306_SWITCH_CHARGER_ENABLE,
-  IP5306_SWITCH_CHARGE_CONTROL,
   IP5306_SWITCH_BOOST_ENABLE,
-  IP5306_SWITCH_SOFTWARE_SHUTDOWN, // Pridane pre mapovanie
+  IP5306_SWITCH_BUTTON_SHUTDOWN,
+  IP5306_SWITCH_BOOST_ON_LOAD,
+  IP5306_SWITCH_SHORT_PRESS_BOOST,
 };
 
 enum IP5306SelectType {
@@ -55,68 +56,38 @@ class IP5306 : public PollingComponent, public i2c::I2CDevice {
   void set_battery_level(sensor::Sensor *sensor) { this->battery_level_ = sensor; }
   void set_current_sensor(sensor::Sensor *current_sensor) { this->current_sensor_ = current_sensor; }
   
-  // Novy text sensor
-  void set_load_status_sensor(text_sensor::TextSensor *sensor) { this->load_status_sensor_ = sensor; }
-
   void set_charger_connected(binary_sensor::BinarySensor *sensor) { this->charger_connected_ = sensor; }
   void set_charge_full(binary_sensor::BinarySensor *sensor) { this->charge_full_ = sensor; }
+  
+  void set_load_status_sensor(text_sensor::TextSensor *sensor) { this->load_status_sensor_ = sensor; }
 
-  void set_low_load_shutdown_switch(IP5306Switch *sw) { 
-      sw->set_type(IP5306_SWITCH_LOW_LOAD_SHUTDOWN);
-      this->low_load_shutdown_switch_ = sw; 
-  }
-  void set_charger_enable_switch(IP5306Switch *sw) { 
-      sw->set_type(IP5306_SWITCH_CHARGER_ENABLE);
-      this->charger_enable_switch_ = sw; 
-  }
-  void set_charge_control_switch(IP5306Switch *sw) { 
-      sw->set_type(IP5306_SWITCH_CHARGE_CONTROL);
-      this->charge_control_switch_ = sw; 
-  }
-  void set_boost_control_switch(IP5306Switch *sw) { 
-      sw->set_type(IP5306_SWITCH_BOOST_ENABLE);
-      this->boost_control_switch_ = sw; 
-  }
-  void set_software_shutdown_switch(IP5306Switch *sw) { 
-      sw->set_type(IP5306_SWITCH_SOFTWARE_SHUTDOWN);
-      this->software_shutdown_switch_ = sw; 
+  void set_switch(IP5306Switch *sw, IP5306SwitchType type) {
+    sw->set_type(type);
+    this->switches_.push_back(sw);
   }
 
-  void set_load_shutdown_time_select(IP5306Select *sel) {
-    sel->set_type(IP5306_SELECT_LOAD_SHUTDOWN_TIME);
-    this->load_shutdown_time_select_ = sel;
-  }
-  void set_charge_cutoff_voltage_select(IP5306Select *sel) {
-    sel->set_type(IP5306_SELECT_CHARGE_CUTOFF_VOLTAGE);
-    this->charge_cutoff_voltage_select_ = sel;
-  }
-  void set_charge_termination_current_select(IP5306Select *sel) {
-    sel->set_type(IP5306_SELECT_CHARGE_TERMINATION_CURRENT);
-    this->charge_termination_current_select_ = sel;
+  void set_select(IP5306Select *sel, IP5306SelectType type) {
+    sel->set_type(type);
+    this->selects_.push_back(sel);
   }
 
   void write_register_bit(uint8_t reg, uint8_t mask, bool value);
   void write_register_bits(uint8_t reg, uint8_t mask, uint8_t shift, uint8_t value);
 
  private:
-  sensor::Sensor *current_sensor_{nullptr};
   sensor::Sensor *battery_level_{nullptr};
-  text_sensor::TextSensor *load_status_sensor_{nullptr};
-  
+  sensor::Sensor *current_sensor_{nullptr};
   binary_sensor::BinarySensor *charger_connected_{nullptr};
   binary_sensor::BinarySensor *charge_full_{nullptr};
-  
-  IP5306Switch *low_load_shutdown_switch_{nullptr};
-  IP5306Switch *charger_enable_switch_{nullptr};
-  IP5306Switch *charge_control_switch_{nullptr};
-  IP5306Switch *boost_control_switch_{nullptr};
-  IP5306Switch *software_shutdown_switch_{nullptr};
-  
-  IP5306Select *load_shutdown_time_select_{nullptr};
-  IP5306Select *charge_cutoff_voltage_select_{nullptr};
-  IP5306Select *charge_termination_current_select_{nullptr};
-  
+  text_sensor::TextSensor *load_status_sensor_{nullptr};
+
+  std::vector<IP5306Switch *> switches_;
+  std::vector<IP5306Select *> selects_;
+
+  // Premenne pre ukladanie posledneho znameho stavu (aby sme nespamovali log)
   float last_battery_level_{-1};
+  float last_current_{-1};
+  std::string last_load_status_{""};
 };
 
 }  // namespace ip5306
